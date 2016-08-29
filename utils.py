5ead95ac372ad3007e9f89ae2d83273839558cab
Eric B <ebigelow@u.rochester.edu>
2016-08-29 18:03:33 -0400

    Use VRD data instead of jcjohnson scene graphs.

--- utils.py
+++ utils.py
@@ -192,6 +192,78 @@ def sg_to_triplets(scene_graphs, word2idx):
     return D
 
 
+# TODO for mat data
+def mat_to_triplets(mat, word2idx):
+    D = []
+
+    for datum in mat_data:
+        if not hasattr(datum, 'relationship'):
+            continue
+        img_rels = datum.relationship
+        if not hasattr(img_rels, '__getitem__'):
+            if not all(i in dir(img_rels) for i in ['objBox', 'phrase', 'subBox']):
+                print 'skipping relation, dir contains:', [_ for _ in dir(img_rels) if '_' not in _]
+                continue
+            img_rels = [img_rels]
+
+        img = imread(img_dir + datum.filename)
+        # print img_dir+datum.filename; print img.shape
+        img_id = datum.photo_id
+        get_objid = lambda x: str(img_id) + '_' + str(x.id)
+
+        for rel in img_rels:
+            R = convert_rel(rel, word2idx)
+            O1 = get_objid(rel.subject)
+            O2 = get_objid(rel.object)
+            D.append((R, O1, O2))
+
+    return D
+
+
+
+
+
+def get_data2(mat_data, obj_dict, rel_dict, img_dir, mean_file='mean.npy'):
+    obj_data = []
+    rel_data = []
+
+    for datum in mat_data:
+        if not hasattr(datum, 'relationship'):
+            #print 'skipping image {}, no relationship'.format(img_dir + datum.filename)
+            continue
+        img_rels = datum.relationship
+        if not hasattr(img_rels, '__getitem__'):
+            if not all(i in dir(img_rels) for i in ['objBox', 'phrase', 'subBox']):
+                print 'skipping relation, dir contains:', [_ for _ in dir(img_rels) if '_' not in _]
+                continue
+            img_rels = [img_rels]
+
+        img = imread(img_dir + datum.filename)
+        # print img_dir+datum.filename; print img.shape
+        for rel in img_rels:
+            ymin1, ymax1, xmin1, xmax1 = rel.subBox
+            ymin2, ymax2, xmin2, xmax2 = rel.objBox
+            ymin3, ymax3, xmin3, xmax3 = (min(ymin1, ymin2), max(ymax1, ymax2),
+                                          min(xmin1, xmin2), max(xmax1, xmax2))
+            h1, w1 = (ymax1 - ymin1, xmax1 - xmin1)
+            h2, w2 = (ymax2 - ymin2, xmax2 - xmin2)
+            h3, w3 = (ymax3 - ymin3, xmax3 - xmin3)
+
+            img1 = square_crop(img, 224, xmin1, ymin1, w1, h1) - np.load(mean_file)
+            img2 = square_crop(img, 224, xmin2, ymin2, w2, h2) - np.load(mean_file)
+            img3 = square_crop(img, 224, xmin3, ymin3, w3, h3) - np.load(mean_file)
+
+            s,v,o = rel.phrase
+            sd = np.zeros((100)); sd[obj_dict[s]] = 1
+            od = np.zeros((100)); od[obj_dict[o]] = 1
+            vd = np.zeros((70));  vd[rel_dict[v]] = 1
+            obj_data.append((img1, sd))
+            obj_data.append((img2, od))
+            rel_data.append((img3, vd))
+
+    return obj_data, rel_data
+
+
 
 # ---------------------------------------------------------------------------------------------------------
 # Scene graph stuff
--- vrd_model.py
+++ vrd_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import make_w2v, loadmat, sg_to_triplets
+from utils import make_w2v, loadmat, mat_to_triplets
 from model import Model
 
 import sys
@@ -15,25 +15,24 @@ rel_mat   = 'data/vrd/predicate.mat'
 w2v_file  = 'data/word2vec/w2v.npy'
 obj_file  = 'data/models/objnet/obj_probs.npy'
 rel_file  = 'data/models/relnet/rel_feats.npy'
-json_file = 'data/vrd/json/train.json'
+train_mat = 'data/vrd/annotation_train.mat'
 
 # --------------------------------------------------------------------------------------------------
 # Load data
 
-obj_dict = {r:i for i,r in enumerate(loadmat(obj_mat)['objectListN'])}
-rel_dict = {r:i for i,r in enumerate(loadmat(rel_mat)['predicate'])}
-word2idx = {'obj':obj_dict, 'rel':rel_dict}
-
 #w2v = make_w2v(word2idx, w2v_bin='data/word2vec/GoogleNews-vectors-negative300.bin')
 #np.save(w2v_file, w2v)
+obj_dict = {r:i for i,r in enumerate(loadmat(obj_mat)['objectListN'])}
+rel_dict = {r:i for i,r in enumerate(loadmat(rel_mat)['predicate'])}
+
+word2idx = {'obj':obj_dict, 'rel':rel_dict}
 w2v = np.load(w2v_file)
 
 obj_probs = np.load(obj_file).item()
 rel_feats = np.load(rel_file).item()
 
-scene_graphs = vg.GetSceneGraphsVRD(json_file)
-import ipdb; ipdb.set_trace()
-D = sg_to_triplets(scene_graphs, word2idx)
+mat = loadmat(train_mat)['annotation_train']
+D = mat_to_triplets(train_mat, word2idx)
 
 # --------------------------------------------------------------------------------------------------
 # Run model
