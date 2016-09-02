4738d649b7db6f9be8774b6a2a73d2126731cf8c
Eric B <ebigelow@u.rochester.edu>
2016-09-02 11:18:13 -0400

    Made weight updates/uses more object-oriented.

--- model.py
+++ model.py
@@ -99,6 +99,15 @@ class Model:
         R_pairs = [(R_rand(), R_rand()) for n in range(S)]
         self.R_samples = R_pairs
 
+    def update(self, W=None, b=None, Z=None, s=None):
+        if W is not None:
+            self.W = W
+        if b is not None:
+            self.b = b
+        if Z is not None:
+            self.Z = Z
+        if s is not None:
+            self.s = s
 
     # -------------------------------------------------------------------------------------------------------
 
@@ -108,33 +117,35 @@ class Model:
                cosine(w2v[R1[1]],   w2v[R2[1]]) +  \
                cosine(w2v[N+R1[2]], w2v[N+R2[2]])
 
-    def d(self, R1, R2, W, b):
+    def d(self, R1, R2):
         """
         Distance between two predicate triplets.
 
         """
-        d_rel = self.f(R1, W, b) - self.f(R2, W, b)
+        d_rel = self.f(R1) - self.f(R2)
         d_obj = self.w2v_dist(R1, R2)
         d = (d_rel ** 2) / d_obj
         return d if (d > 0) else 1e-10
 
 
-    def f(self, R, W, b):
+    def f(self, R):
         """
         Project relationship `R = <i,j,k>` to scalar space.
 
         """
         i,j,k = R
+        W,b = (self.W, self.b)
         wvec = self.word_vec(i, j)
         return np.dot(W[k].T, wvec) + b[k]
 
-    def V(self, R, O1, O2, Z, s):
+    def V(self, R, O1, O2):
         i,j,k = R
         rel_id = objs_to_reluid(O1, O2)
 
         P_i = self.obj_probs[O1][i]
         P_j = self.obj_probs[O2][j]
         cnn = self.rel_feats[rel_id]
+        Z,s = (self.Z, self.s)
         P_k = np.dot(Z[k], cnn) + s[k]
         return P_i * P_j * P_k
 
@@ -144,9 +155,9 @@ class Model:
         Full list of predictions `R`, sorted by confidence
 
         """
-        N,K,W,b,Z,s = (self.n, self.k, self.W, self.b, self.Z, self.s)
+        N,K = (self.n, self.k)
         Rs = [(i,j,k) for i in range(N) for j in range(N) for k in range(K)]
-        M = sorted(Rs, key=lambda R: -self.V(R,O1,O2,Z,s) * self.f(R,W,b))
+        M = sorted(Rs, key=lambda R: -self.V(R,O1,O2) * self.f(R))
         return M
 
     def compute_accuracy(self, GT, k=None):
@@ -198,7 +209,6 @@ class Model:
         """
         obj_probs, rel_feats, w2v = (self.obj_probs, self.rel_feats, self.w2v)
         V, f, d = (self.V, self.f, self.d)
-        W, b, Z, s = (self.W, self.b, self.Z, self.s)
         cost_prev = 0.0
 
         for epoch in range(self.max_iters):
@@ -211,36 +221,40 @@ class Model:
 
                 # Get (R, O1, O2) that maximizes term in equation 6
                 D_ = [(R_,O1_,O2_) for R_,O1_,O2_ in D if (R_ != R) and (O1_ != O1 or O2_ != O2)]
-                M = sorted(D_, key=lambda (R,O1,O2): V(R,O1,O2,Z,s) * f(R,W,b))
+                M = sorted(D_, key=lambda (R,O1,O2): V(R,O1,O2) * f(R))
                 R_,O1_,O2_ = M[0]
 
                 i,j,k = R
                 i_,j_,k_ = R_
 
                 # Compute value for ` max{cost, 0} ` in equation 6
-                cost = max(1. - V(R,O1,O2,Z,s) * f(R,W,b) + V(R_,O1_,O2_,Z,s) * f(R_,W,b), 0.)
+                cost = max(1. - V(R,O1,O2) * f(R) + V(R_,O1_,O2_) * f(R_), 0.)
                 mc  += cost / len(D)
 
 
                 # Even epochs --> update W,b
                 if epoch % 2 == 0:
                     # Equation 5
+                    lr = self.learning_rate * self.lamb1
                     for R_2, O1_2, O2_2 in D:
                         i_2,j_2,k_2 = R_2
-                        if f(R_2, W, b) - f(R, W, b) > 0:
-                            W[k]   -= self.learning_rate * self.lamb1 * np.concatenate((w2v[i],  w2v[j]))
-                            b[k]   -= self.learning_rate * self.lamb1
-                            W[k_2] += self.learning_rate * self.lamb1 * np.concatenate((w2v[i_2], w2v[j_2]))
-                            b[k_2] += self.learning_rate * self.lamb1
+                        if f(R_2) - f(R) > 0:
+                            W[k]   -= lr * np.concatenate((w2v[i], w2v[j]))
+                            b[k]   -= lr
+                            W[k_2] += lr * np.concatenate((w2v[i_2], w2v[j_2]))
+                            b[k_2] += lr
+                            self.update(W=W, b=b)
 
                     # Equation 6
                     if cost > 0:
-                        v = V(R,O1,O2,Z,s)
+                        lr = self.learning_rate
+                        v = V(R,O1,O2)
                         W[k]  -= self.learning_rate * v * np.concatenate((w2v[i], w2v[j]))
                         b[k]  -= self.learning_rate * v
-                        v_ = V(R_,O1_,O2_,Z,s)
+                        v_ = V(R_,O1_,O2_)
                         W[k_] += self.learning_rate * v_ * np.concatenate((w2v[i_], w2v[j_]))
                         b[k_] += self.learning_rate * v_
+                        self.update(W=W, b=b)
 
 
                 # Odd epochs --> update Z,s
@@ -249,20 +263,22 @@ class Model:
                     if cost > 0:
                         id_rel  = objs_to_reluid(O1, O2)
                         id_rel_ = objs_to_reluid(O1_, O2_)
+                        lr = self.learning_rate
 
-                        Z[k]  -= self.learning_rate * f(R,W,b) * obj_probs[O1][i] * obj_probs[O2][j] * rel_feats[id_rel]
-                        s[k]  -= self.learning_rate * f(R,W,b) * obj_probs[O1][i] * obj_probs[O2][j]
-                        Z[k_] += self.learning_rate * f(R_,W,b)  * obj_probs[O1_][i_] * obj_probs[O2_][j_] * rel_feats[id_rel_]
-                        s[k_] += self.learning_rate * f(R_,W,b)  * obj_probs[O1_][i_] * obj_probs[O2_][j_]
+                        Z[k]  -= lr * f(R)  * obj_probs[O1][i]   * obj_probs[O2][j]   * rel_feats[id_rel]
+                        s[k]  -= lr * f(R)  * obj_probs[O1][i]   * obj_probs[O2][j]
+                        Z[k_] += lr * f(R_) * obj_probs[O1_][i_] * obj_probs[O2_][j_] * rel_feats[id_rel_]
+                        s[k_] += lr * f(R_) * obj_probs[O1_][i_] * obj_probs[O2_][j_]
+                        self.update(Z=Z, s=s)
 
             # # Equation 4
             # if epoch % 2 == 0:
-            #     dKfun = sum(    (2. / d(R,R_,W,b)) *
-            #                     (f(R,W,b) - f(R_,W,b)) *
+            #     dKfun = sum(    (2. / d(R,R_)) *
+            #                     (f(R,W,b) - f(R_)) *
             #                     (self.word_vec(*R[:-1]) - self.word_vec(*R_[:-1]))
             #                 for R,R_ in self.R_samples )
             #
-            #     Kfun = lambda R, R_: (f(R,W,b) - f(R_,W,b))**2 / d(R,R_,W,b)
+            #     Kfun = lambda R, R_: (f(R) - f(R_))**2 / d(R,R_)
             #     Ksum = sum(Kfun(R,R_) for R, R_ in self.R_samples)
             #     nr = self.num_samples
             #     dK_dW = ((2.0 - nr) / nr) * Ksum * dKfun
@@ -271,8 +287,8 @@ class Model:
             #     # TODO: if (1 - v + v' > 0) : update
             #
             #     W += self.learning_rate * dK_dW * self.lamb2
+            #     self.update(W=W, b=b)
 
-            self.W, self.b, self.Z, self.s = (W, b, Z, s)
             self.save_weights(save_file)
 
             final_obj = cost + self.lamb1 * self.L(D)
@@ -334,7 +350,6 @@ class Model:
     def SGD_update(self, R, O1, O2):
         obj_probs, rel_feats, w2v = (self.obj_probs, self.rel_feats, self.w2v)
         V, f, d = (self.V, self.f, self.d)
-        W, b, Z, s = (self.W, self.b, self.Z, self.s)
 
         # Get 2nd data point that maximizes equation ??? TODO
         D_ = [(R_,O1_,O2_) for R_,O1_,O2_ in D if (R_ != R) and (O1_ != O1 or O2_ != O2)]
@@ -396,7 +411,7 @@ class Model:
 
 
     # -------------------------------------------------------------------------------------------------------
-    # Unused
+    # Original Equations
 
     def K(self):
         """  UNUSED
@@ -415,29 +430,32 @@ class Model:
 
         """
         Rs, O1s, O2s = zip(*D)
-        fn = lambda R1, R2: max(self.f(R1, self.W, self.b) - self.f(R2, self.W, self.b) + 1, 0)
+        fn = lambda R1, R2: max(self.f(R1) - self.f(R2) + 1, 0)
         return sum(fn(R1, R2) for R1 in Rs for R2 in Rs)
+     
+    # def C(self, D):
+    #     """  UNUSED
+    #     Rank loss function
+    #
+    #     TODO -- wrong
+    #
+    #     """
+    #     V,f = (self.V, self.f)
+    #     C = 0.0
+    #     for R, O1, O2 in D:
+    #         c = max(V(*d2) * f(*d2[3:]) for d2 in img_data if  \
+    #                 (d2[5] != d2[5]) and ((d1[3] != d2[3]) or (d1[4] != d2[4])))
+    #         C += max(1 + V(*d1) * f(*d1[3:]) * c, 0)
+    #     return C
 
-    def C(self, img_data):
-        """  UNUSED
-        Rank loss function
-
-        """
-        C = 0.0
-        for d1 in img_data:
-            c = max(V(*d2) * f(*d2[3:]) for d2 in img_data if  \
-                    (d2[5] != d2[5]) and ((d1[3] != d2[3]) or (d1[4] != d2[4])))
-            C += max(1 + V(*d1) * f(*d1[3:]) * c, 0)
-        return C
-
-    def loss(self, img_data):
+    def loss(self, dir):
         """  UNUSED
         Final objective loss function.
 
         """
-        C = self.C(img_data)
+        C = self.C(D)
         K = self.lamb1 * self.K()
-        L = self.lamb2 * L(img_data)
+        L = self.lamb2 * L(D)
         return C + K + L
 
     def predict_R(self, O1, O2):
