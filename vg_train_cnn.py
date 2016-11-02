f9088bb09a6570a3c909441f1b12ffacda11e7bb
Eric Bigelow <ebigelow@node1x6x2b.cs.rochester.edu>
2016-11-01 22:02:07 -0400

    lots of old changes from node b. This will probably get messy.

--- vg_train_cnn.py
+++ vg_train_cnn.py
@@ -1,7 +1,8 @@
-import tensorflow as tf
 from utils import load_sg_batcher
+import tensorflow as tf
 from vgg16 import CustomVgg16
 import numpy as np
+from tqdm import tqdm
 
 tf.app.flags.DEFINE_float('gpu_mem_fraction', 0.9, '')
 
@@ -9,6 +10,7 @@ tf.app.flags.DEFINE_string('which_net', 'objnet', '')
 tf.app.flags.DEFINE_string('init_path', 'data/models/vgg16.npy', 'Initial weights')
 tf.app.flags.DEFINE_string('save_path', 'data/models/objnet/vgg16_vg_trained.npy', 'Save weights to this file')
 
+tf.app.flags.DEFINE_float('learning_rate', 0.01,   '')
 tf.app.flags.DEFINE_integer('batch_size',  10,    '')
 tf.app.flags.DEFINE_integer('data_epochs', 20,    '')
 tf.app.flags.DEFINE_integer('meta_epochs', 20,    '')
@@ -16,9 +18,9 @@ tf.app.flags.DEFINE_integer('train_idx',   90000, '')
 
 tf.app.flags.DEFINE_string('img_dir',     'data/vg/images/', '')
 tf.app.flags.DEFINE_string('img_mean',    'mean.npy',        '')
-tf.app.flags.DEFINE_string('label_dict',  'data/vg/json/label_dict.npy', '')
-tf.app.flags.DEFINE_string('json_dir',    'data/vg/json/short/',         '')
-tf.app.flags.DEFINE_string('json_id_dir', 'data/vg/json/short/by-id/',   '')
+tf.app.flags.DEFINE_string('label_dict',  'data/vg/json/vg_short/label_dict.npy', '')
+tf.app.flags.DEFINE_string('json_dir',    'data/vg/json/vg_short/',         '')
+tf.app.flags.DEFINE_string('json_id_dir', 'data/vg/json/vg_short/by-id/',   '')
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     output_size = n if FLAGS.which_net == 'objnet' else k
     net.build(images_var, train=True, output_size=output_size)
 
-    ground_truth, cost, train_op = net.get_train_op()
+    ground_truth, cost, train_op = net.get_train_op(learning_rate=FLAGS.learning_rate)
     accuracy = net.get_accuracy(ground_truth)
 
     best_acc = 0.0
@@ -53,22 +55,25 @@ if __name__ == '__main__':
     }
     test_params = train_params.copy()
     test_params['start_idx'] = FLAGS.train_idx
-    test_params['end_idx']   = -1
+    test_params['end_idx']   = FLAGS.train_idx + 1000
 
-    #TODO: CPU ONLY
-    # with session_init() as sess:
-    with tf.Session() as sess:
+    gpu_fraction = tf.GPUOptions(per_process_gpu_memory_fraction=FLAGS.gpu_mem_fraction)
+    session_init = lambda: tf.Session(config=tf.ConfigProto(gpu_options=(gpu_fraction)))
+
+    #import ipdb; ipdb.set_trace()
+    with session_init() as sess:
         tf.initialize_all_variables().run()
 
         for e in range(FLAGS.meta_epochs):
             print 'Beginning epoch {}'.format(e)
             data_batcher = load_sg_batcher(**train_params)
-            for db, data_batch in enumerate(data_batcher):
+            for db, data_batch in tqdm(enumerate(data_batcher)):
 
-                for b, (images, labels) in enumerate(data_batch):
+                for b, (images, labels) in tqdm(enumerate(data_batch)):
                     feed_dict = {ground_truth: labels, images_var: images}
                     sess.run(train_op, feed_dict=feed_dict)
 
+            print '\tBegin testing!'
             test_batcher = load_sg_batcher(**test_params)
             accs = []
             for test_batch in test_batcher:
@@ -78,7 +83,7 @@ if __name__ == '__main__':
                     accs.append(batch_acc)
 
             acc = np.mean(accs)
-            print ' => epoch {} acurracy: {}'.format(e, acc)
+            print ' => epoch {} acurracy: {}'.format(db, acc)
             if acc > best_acc:
                 best_acc = acc
                 net.save_npy(sess, file_path=FLAGS.save_path)
